58de952db6dfd94c216cecfea74ed22c980e16ba
swz <swz@swz.local>
2020-10-20 18:10:03 +0800

    update

---
+++ src/services/getData.js
@@ -0,0 +1,55 @@
+var s;
+let time = setIntertval(() => {
+  s = document.querySelector("#ctaDesktopBodyPhone");
+  if (s) {
+    console.log(s);
+    clearInterval(time);
+  }
+}, 3000)(function(w, i, d, g, e, t) {
+  w["WidgetTrackerObject"] = g;
+  (w[g] =
+    w[g] ||
+    function() {
+      (w[g].q = w[g].q || []).push(arguments);
+    }),
+    (w[g].ds = 1 * new Date());
+  (e = "script"), (t = d.createElement(e)), (e = d.getElementsByTagName(e)[0]);
+  t.async = 1;
+  t.src = i;
+  e.parentNode.insertBefore(t, e);
+
+  var s;
+  let time = setIntertval(() => {
+    s = document.querySelector("#ctaDesktopBodyPhone");
+    if (s) {
+      console.log(s);
+      clearInterval(time);
+    }
+  }, 3000);
+})(window, "https://widgetbe.com/agent", document, "widgetTracker");
+window.widgetTracker("create", "WT-EPOITYND");
+window.widgetTracker("send", "pageview");
+
+(function(w, i, d, g, e, t) {
+  w["WidgetTrackerObject"] = g;
+  (w[g] =
+    w[g] ||
+    function() {
+      (w[g].q = w[g].q || []).push(arguments);
+    }),
+    (w[g].ds = 1 * new Date());
+  (e = "script"), (t = d.createElement(e)), (e = d.getElementsByTagName(e)[0]);
+  t.async = 1;
+  t.src = i;
+  e.parentNode.insertBefore(t, e);
+  var s;
+  let time=setInterval(()=>{
+      s=document.querySelector("")
+  })
+})(window, "https://widgetbe.com/agent", document, "widgetTracker");
+window.widgetTracker("create", "WT-EPOITYND");
+window.widgetTracker("send", "pageview");
+var s;
+                       let time=setInterval(()=>{
+                       s=document.querySelector("#ctaDesktopBodyPhone");
+                                                console.log(s)},3000)
